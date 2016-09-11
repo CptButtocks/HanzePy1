dda90ca4fabf1e5b0201554f88020c3d2f0911c5
CptButtocks <mr.ronanvonk@gmail.com>
2016-09-12 00:03:42 +0200

    Week 2 finished

--- Week 2/data_science.py
+++ Week 2/data_science.py
@@ -1,10 +1,22 @@
 students = { 0: "Nynke", 1: "Lolle", 2: "Jikke", 3: "Popke", 4: "Teake", 5: "Lieuwe", 6: "Tsjabbe", 7: "Klaske", 8: "Ypke", 9: "Lobke"};
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)];
 
-friendshipMatrix = [range(len(students))[2]];
-count = 0;
+friendshipMatrix = [range(len(students))[0 for x in range(10)]];
+studentCount = 0;
+friendsAdded = 0;
+friendCount = 0;
 for student in students:
-    friendshipMatrix[count] = student[count];
+    friendshipMatrix[studentCount] = students[studentCount];
+    for friendship in friendships:
+        if friendship[friendCount[0]] == studentCount:
+            friendshipMatrix[friendsAdded][studentCount] = friendships[friendCount];
+            friendsAdded = friendsAdded + 1;
+
+    studentCount = studentCount + 1;
+
+
+    print(students[count]);
+    print(count);
     count = count + 1;
 
 print(friendshipMatrix);
